66c21653eb3d86f70b1d106b4e0fbf35cb980384
TristanLemire <tristan.lemire.sio@gmail.com>
2021-04-19 13:01:38 +0200

    Style tabBar

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {TodoNavigation} from './src/navigation/TodoNavigation'
-import {AlbumNavigation} from './src/navigation/AlbumNavigation'
+import {TodoNavigation} from './src/navigation/TodoNavigation';
+import {AlbumNavigation} from './src/navigation/AlbumNavigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const Tab = createBottomTabNavigator();
@@ -10,7 +11,32 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Todo') {
+              iconName = 'ios-list'
+
+            } else if (route.name === 'Album') {
+              iconName = focused ? 'ios-albums-sharp':'ios-albums-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          style: {
+            alignItems: "center",
+            borderTopWidth: 0.5,
+          },
+          labelStyle: {
+            fontSize: 12,
+          },
+        }} >
         <Tab.Screen name="Todo" component={TodoNavigation} />
         <Tab.Screen name="Album" component={AlbumNavigation} />
       </Tab.Navigator>
