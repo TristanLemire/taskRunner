d9095a46d4e023a08dab8f23293c19f87dccb87b
Thomasevano <thomas-evano@hotmail.fr>
2021-04-19 16:42:20 +0200

    Add User view information

--- src/subApps/User/UserScreen.jsx
+++ src/subApps/User/UserScreen.jsx
@@ -5,6 +5,28 @@ export function UserScreen(props) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>{props.route.params.user.name}</Text>
+      <Text>{props.route.params.user.username}</Text>
+      
+      <Text>Personnal Info</Text>
+      
+      <Text>{props.route.params.user.email}</Text>
+      <Text>{props.route.params.user.address.street} {props.route.params.user.address.suite} {props.route.params.user.address.city} {props.route.params.user.address.zipcode}</Text>
+      <Text>{props.route.params.user.phone}</Text>
+      <Text>{props.route.params.user.website}</Text>
+      
+      <Text>Company</Text>
+      
+      <Text>{props.route.params.user.company.name}</Text>
+      <Text>{props.route.params.user.company.catchPhrase}</Text>
+      <Text>{props.route.params.user.company.bs}</Text>
+      
+      
+      <Button
+        onPress={() => props.route.params.onChange(false)}
+        title="Select user"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
