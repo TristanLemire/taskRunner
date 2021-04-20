ac42b36a9cc7cea8fef7b36dbec4d347abc095e3
Kalani Marquand <43987061+KalaniM@users.noreply.github.com>
2021-04-20 11:36:28 +0200

    chore(userscreen): destructuring assignement

--- src/subApps/User/UserScreen.jsx
+++ src/subApps/User/UserScreen.jsx
@@ -5,10 +5,37 @@ import { Avatar } from "react-native-elements";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 export const UserScreen = (props) => {
-  const user = props.route.params.user;
-  const lat = parseInt(user.address.geo.lat, 10);
-  const lng = parseInt(user.address.geo.lng, 10);
 
+const {
+  route: {
+    params: {
+      user: {
+        username,
+        email,
+        phone,
+        website,
+        company: {
+          name,
+          catchPhrase,
+          bs,
+        },
+        address: {
+          street,
+          suite,
+          city,
+          zipcode,
+          geo: {
+            lat,
+            lng,
+          }
+        }
+      }
+    }
+  }
+} = props;
+
+  const latitude = parseInt(lat, 10);
+  const longitude = parseInt(lng, 10);
   return (
     <View
       style={{
@@ -25,41 +52,28 @@ export const UserScreen = (props) => {
           size="large"
           source={{ uri: user.image }}
         />
-        <Text>{user.username}</Text>
+        <Text>{username}</Text>
       </View>
 
       <Text>Personnal Info</Text>
       <View style={styles.userInfo}>
-        <Text>
-          <Ionicons name="mail-outline" size={22} color="#ff7A00" />
-          {user.email}
-        </Text>
-        <Text>
-          <Ionicons name="location-outline" size={22} color="#ff7A00" />
-          {user.address.street} {user.address.suite} {user.address.city}{" "}
-          {user.address.zipcode}
-        </Text>
-        <Text>
-          <Ionicons name="phone-portrait-outline" size={22} color="#ff7A00" />
-          {user.phone}
-        </Text>
-        <Text>
-          <Ionicons name="globe-outline" size={22} color="#ff7A00" />
-          {user.website}
-        </Text>
+        <Text><Ionicons name="mail-outline" size={22} color="#ff7A00" />{email}</Text>
+        <Text><Ionicons name="location-outline" size={22} color="#ff7A00" />{street} {suite} {city} {zipcode}</Text>
+        <Text><Ionicons name="phone-portrait-outline" size={22} color="#ff7A00" />{phone}</Text>
+        <Text><Ionicons name="globe-outline" size={22} color="#ff7A00" />{website}</Text>
       </View>
 
       <Text>Company</Text>
       <View style={styles.userCompany}>
-        <Text>{user.company.name}</Text>
-        <Text>{user.company.catchPhrase}</Text>
-        <Text>{user.company.bs}</Text>
+        <Text>{name}</Text>
+        <Text>{catchPhrase}</Text>
+        <Text>{bs}</Text>
       </View>
 
       <MapView
         initialRegion={{
-          latitude: lat,
-          longitude: lng,
+          latitude: latitude,
+          longitude: longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
