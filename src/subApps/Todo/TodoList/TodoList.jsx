40816dc5572dbbe6e22249f23ec305c11a527abd
TristanLemire <tristan.lemire.sio@gmail.com>
2021-04-20 14:55:48 +0200

    Custom component checkbox

--- src/subApps/Todo/TodoList/TodoList.jsx
+++ src/subApps/Todo/TodoList/TodoList.jsx
@@ -1,42 +1,34 @@
 import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
-import { CheckBox } from 'react-native-elements'
-
+import { View, StyleSheet, Dimensions } from "react-native";
+import { CheckBox } from "react-native-elements";
 
 export function TodoList(props) {
-    const [checked, setChecked] = useState(false);
-    console.log("props", props.todoTitle);
+  const [checked, setChecked] = useState(false);
+  console.log("props", props.todoTitle);
 
-return (
-    <View style={styles.viewList}>     
-        <CheckBox
-            left
-            title={props.todoTitle}
-            checkedIcon='dot-circle-o'
-            uncheckedIcon='circle-o'
-            checked={checked}
-            containerStyle={styles.task}
-        />
-    </View>
-    );
+  return (
+    <CheckBox
+      title={props.todoTitle}
+      checkedIcon="dot-circle-o"
+      uncheckedIcon="circle-o"
+      checked={checked}
+      containerStyle={styles.task}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    viewList : {
-        width: "100%",
-        height: "4em",
-    },
-    task:{
-        height: "100%",
-        width: "100%",
-        backgroundColor: "#FFFFFF",
-        borderBottomColor: "#DDDDDD",
-        borderBottomWidth: 1, 
-        borderTopColor: "#DDDDDD",
-        borderTopWidth: 1,
-        marginLeft: 0,
-        padding: "1.125em"
-    }
+  task: {
+    // backgroundColor: "#FFFFFF",
+    // borderBottomColor: "#DDDDDD",
+    // borderBottomWidth: 1,
+    // borderTopColor: "#DDDDDD",
+    // borderTopWidth: 1,
+    // marginLeft: 0,
+    // padding: 1.125,
+    width: Dimensions.get("window").width,
+    margin: 0,
+  },
 });
 
 export default TodoList;
--- src/subApps/Todo/TodoScreen.jsx
+++ src/subApps/Todo/TodoScreen.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/AntDesign';
-import TodoList from './TodoList/TodoList';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ScrollView, FlatList } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/AntDesign";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export function TodoScreen(props) {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [checked, setChecked] = useState(false);
 
   const userId = props.route.params.user.id;
 
@@ -13,22 +15,51 @@ export function TodoScreen(props) {
     fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
       .then((response) => response.json())
       .then((json) => setTodos(json));
-      console.log("todos: ", todos)
+  };
 
-  }
+  useEffect(() => {
+    postTodo();
+  }, []);
+
+  console.log("todos: ", todos);
   return (
-    <View style={styles.todoView}>
+    <View style={{ backgroundColor: "white" }}>
       <ScrollView>
-        {
-          todos.map((item) =>{
-            {console.log("todos title: ", item.title)}
-            return (<TodoList 
-            todoTitle={item.title}
-          />)
-          })
-        }
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.checkbox}
+              onPress={() => {
+                console.log(
+                  "tache fini: PA ici tu ferra un call api pour dire que cette tache est valide ou invalide si elle est deja validé",
+                  postTodo()
+                );
+              }}
+            >
+              <Ionicons
+                name={
+                  item.completed
+                    ? "ios-checkmark-circle"
+                    : "ios-ellipse-outline"
+                }
+                size={35}
+                color={item.completed ? "#95DC2E" : "#dddddd"}
+                style={{ marginRight: 5 }}
+              />
+              <Text
+                style={
+                  item.completed
+                    ? { textDecorationLine: "line-through", color: "#c7c7c7" }
+                    : { color: "#181818" }
+                }
+              >
+                {item.title}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
       </ScrollView>
-        
       <View style={styles.buttonContainer}>
         <Button
           buttonStyle={styles.addButton}
@@ -40,30 +71,43 @@ export function TodoScreen(props) {
               style={styles.addIcon}
             />
           }
-          onPress={postTodo}
+          onPress={() =>
+            console.log(
+              "PA ici tu fais un fonction qui t'ouvre la modal d'ajout d'une nouvelle tache"
+            )
+          }
         />
-    </View>
       </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  todoView: {
-    flex: 1,
-    alignItems: "flex-end",
-    backgroundColor: "#FFFFFF",
+  addIcon: {
+    color: "#FF7A00",
   },
-  addIcon : {
-    color: "#FF7A00"
-  },
-
   addButton: {
     backgroundColor: "#FFFFFF",
+    borderRadius: 50,
+    margin: 0,
+    padding: 0,
     borderWidth: 0,
-  }, 
-  buttonContainer:{
-    position: "relative",
-    right: "10px",
-    bottom: "5px",
-  }
+  },
+  buttonContainer: {
+    position: "absolute",
+
+    right: 15,
+    bottom: 15,
+  },
+  task: {
+    margin: 0,
+    padding: 20,
+  },
+  checkbox: {
+    flexDirection: "row",
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    padding: 10,
+    alignItems: "center",
+  },
 });
