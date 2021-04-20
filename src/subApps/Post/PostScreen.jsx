cca70c72e541a33f29eeda86cf87e2f18cf2a806
Kalani Marquand <43987061+KalaniM@users.noreply.github.com>
2021-04-20 14:57:48 +0200

    feat(postScreen): create posts and styling

--- src/subApps/Post/PostScreen.jsx
+++ src/subApps/Post/PostScreen.jsx
@@ -1,11 +1,46 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export function PostScreen(props) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Post!</Text>
-      <Text>{props.route.params.user.name}</Text>
+    <View style={styles.page}>
+
+      <View style={styles.container}>
+      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
+      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
+      </View>
+
+      <View style={styles.container}>
+      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
+      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
+      </View>
+      <View style={styles.container}>
+      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
+      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
+      </View>
+      <View style={styles.container}>
+      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
+      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'space-evenly',
+    maxHeight: 120,
+  },
+  containerTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  containerText: {
+    fontSize: 14,
+  }
+});
