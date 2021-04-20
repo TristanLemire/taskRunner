0f0344be6552937cf7c5faabb84815fa9d9b71c7
Kalani Marquand <43987061+KalaniM@users.noreply.github.com>
2021-04-20 15:21:35 +0200

    feat(Post): create component Post and style it

---
+++ src/subApps/Post/Components/Post.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { ListItem } from "react-native-elements";
+
+export function Post(props) {
+  const style = PostStyle();
+
+  return (
+    <ListItem bottomDivider pad={16}>
+      <ListItem.Content>
+        <ListItem.Title>
+          <Text style={style.userName}>Lorem dolor ipsum</Text>
+        </ListItem.Title>
+
+        <ListItem.Subtitle style={style.subtitle}>
+          <Text style={style.userEmail}>
+            ullam et saepe reiciendis voluptatem adipisci\nsit amet autem
+            assumenda provident rerum...
+          </Text>
+        </ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  );
+}
+
+const PostStyle = () =>
+  StyleSheet.create({
+    userName: {
+      color: "#20232a",
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+    subtitle: {
+      paddingTop: 8,
+    },
+    userEmail: {
+      fontSize: 12,
+    },
+  });
--- src/subApps/Post/PostScreen.jsx
+++ src/subApps/Post/PostScreen.jsx
@@ -1,28 +1,18 @@
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
+import { Post } from "./Components/Post"
+
 export function PostScreen(props) {
   return (
     <View style={styles.page}>
 
-      <View style={styles.container}>
-      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
-      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
-      </View>
+      <Post>
 
-      <View style={styles.container}>
-      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
-      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
-      </View>
-      <View style={styles.container}>
-      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
-      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
-      </View>
-      <View style={styles.container}>
-      <Text style={styles.containerTitle}>Lorem dolor ipsum</Text>
-      <Text style={styles.containerText}>ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum...</Text>
-      </View>
+      </Post>
+
     </View>
+
   );
 }
 
@@ -30,17 +20,4 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
   },
-  container: {
-    flex: 1,
-    padding: 16,
-    justifyContent: 'space-evenly',
-    maxHeight: 120,
-  },
-  containerTitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  containerText: {
-    fontSize: 14,
-  }
 });
