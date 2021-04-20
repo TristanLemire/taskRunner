496be18eb23c5d886303054bdcb2d4453c9d3db6
TristanLemire <tristan.lemire.sio@gmail.com>
2021-04-20 09:27:06 +0200

    add context

--- src/navigation/UserNavigation.jsx
+++ src/navigation/UserNavigation.jsx
@@ -5,44 +5,54 @@ import { StyleSheet } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import { Consumer } from "../../App";
 const Stack = createStackNavigator();
 
 export function UserNavigation(props) {
   const navigation = useNavigation();
   const UserNavigationStyleContextual = UserNavigationStyle();
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="User"
-        component={UserScreen}
-        initialParams={{
-          user: props.route.params.user,
-        }}
-        options={{
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => props.route.params.onChange(null)}
-              style={UserNavigationStyleContextual.container}
-            >
-              <Ionicons name={"ios-exit-outline"} size={25} color={"#fff"} />
-            </TouchableOpacity>
-          ),
-          title: `${props.route.params.user.name}`,
-          headerStyle: {
-            backgroundColor: "#ff7A00",
-          },
-          headerTitleStyle: {
-            color: "#fff",
-          },
-        }}
-      />
-    </Stack.Navigator>
+    <Consumer>
+      {(value) => (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="User"
+            component={UserScreen}
+            initialParams={{
+              user: props.route.params.user,
+            }}
+            options={{
+              headerRight: () => (
+                <TouchableOpacity
+                  onPress={() => value.onChange(null)}
+                  style={UserNavigationStyleContextual.container}
+                >
+                  <Ionicons
+                    name={"ios-exit-outline"}
+                    size={25}
+                    color={"#fff"}
+                  />
+                </TouchableOpacity>
+              ),
+              title: `${props.route.params.user.name}`,
+              headerStyle: {
+                backgroundColor: "#ff7A00",
+              },
+              headerTitleStyle: {
+                color: "#fff",
+              },
+            }}
+          />
+        </Stack.Navigator>
+      )}
+    </Consumer>
   );
 }
 
 const UserNavigationStyle = () =>
   StyleSheet.create({
     container: {
-      marginRight: 15,
+      marginRight: 24,
     },
   });
