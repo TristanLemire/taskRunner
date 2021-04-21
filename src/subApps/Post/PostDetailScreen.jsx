6f4105350583f8afe377346696e15d531e3b8cf3
Kalani Marquand <43987061+KalaniM@users.noreply.github.com>
2021-04-21 11:15:56 +0200

    feat(PostDetail): implement data

--- src/subApps/Post/PostDetailScreen.jsx
+++ src/subApps/Post/PostDetailScreen.jsx
@@ -1,14 +1,35 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-
-import { useNavigation } from "@react-navigation/native";
+import { Text, View, Button } from "react-native";
+import { ListItem } from "react-native-elements";
 
 export function PostDetailScreen(props) {
-  const navigation = useNavigation();
+  const {
+    route: {
+      params: {
+        post: { id, body, title, userId },
+      },
+    },
+  } = props;
 
+  console.log("body", body);
   return (
     <View>
-      <Text>Post</Text>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Button title="Ajouter un commentaire"></Button>
+
+      <ListItem bottomDivider pad={16}>
+        <Text>Comment</Text>
+        <ListItem.Content>
+          <ListItem.Title>
+            <Text>Comment title</Text>
+          </ListItem.Title>
+
+          <ListItem.Subtitle>
+            <Text>Comment body</Text>
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
     </View>
   );
 }
