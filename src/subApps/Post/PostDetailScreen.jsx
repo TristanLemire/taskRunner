3b26e180b7a0bbf103170533a545aebee0c1d503
Kalani Marquand <43987061+KalaniM@users.noreply.github.com>
2021-04-21 11:27:18 +0200

    feat(PostDetail): fetch data

--- src/subApps/Post/PostDetailScreen.jsx
+++ src/subApps/Post/PostDetailScreen.jsx
@@ -1,17 +1,47 @@
-import * as React from "react";
-import { Text, View, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { ListItem } from "react-native-elements";
 
 export function PostDetailScreen(props) {
   const {
     route: {
       params: {
-        post: { id, body, title, userId },
+        post: { id: postId, body, title, userId },
       },
     },
   } = props;
 
-  console.log("body", body);
+  const [comments, setComments] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const getComments = () => {
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setComments(
+          // console.log("json", json)
+          json.filter((item) => item.postId === props.route.params.post.id)
+        );
+        setIsPending(false);
+      });
+  };
+
+  useEffect(() => {
+    setIsPending(true);
+    getComments();
+  }, []);
+
+  console.log("comments", comments);
+
   return (
     <View>
       <Text>{title}</Text>
@@ -19,10 +49,10 @@ export function PostDetailScreen(props) {
       <Button title="Ajouter un commentaire"></Button>
 
       <ListItem bottomDivider pad={16}>
-        <Text>Comment</Text>
+        <Text>Commentaires</Text>
         <ListItem.Content>
           <ListItem.Title>
-            <Text>Comment title</Text>
+            <Text>name</Text>
           </ListItem.Title>
 
           <ListItem.Subtitle>
@@ -30,6 +60,29 @@ export function PostDetailScreen(props) {
           </ListItem.Subtitle>
         </ListItem.Content>
       </ListItem>
+
+      <ScrollView>
+        {isPending ? (
+          <ActivityIndicator
+            style={{ marginTop: 100 }}
+            size="large"
+            color="#ff7A00"
+          />
+        ) : (
+          <FlatList
+            data={comments}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate("PostDetail", { comment: item })
+                }
+              >
+                <Text>Coucou</Text>
+              </TouchableOpacity>
+            )}
+          />
+        )}
+      </ScrollView>
     </View>
   );
 }
